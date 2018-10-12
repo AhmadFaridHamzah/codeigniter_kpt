e204ee8fae64422484539c0867963e8ea643323f
unknown <ahmadfarid3991@gmail.com>
2018-10-12 09:40:51 +0800

    demo query

--- application/models/M_staff.php
+++ application/models/M_staff.php
@@ -40,4 +40,49 @@ class M_staff extends CI_Model{
         $this->db->where(['staff_id' => $staffid]);
         $this->db->delete('staff');
     }
+
+    function get_staff_store(){
+        $query = $this->db->get('staff');
+        return $query->result_array();
+    }
+
+    function demo_array(){
+        $query = $this->db->get('staff');
+        echo '<br>array:'.$this->db->last_query();
+        return $query->result_array();
+    }
+
+    function demo_object(){
+        $query = $this->db->get('staff');
+        echo '<br>object:'.$this->db->last_query();
+        return $query->result();
+    }
+
+    function democondition(){
+        $staff_id = 7;
+        // $this->db->where('staff_id = 1');
+        $this->db->where(['staff_id' => $staff_id
+        // , 'column2' => 'condition2'
+        ]);
+        $query = $this->db->get('staff');
+        echo '<br>object:'.$this->db->last_query();
+        
+        return $query->row();
+        // return $query->result();
+    }
+
+    function demodirectquery(){
+        $query = $this->db->query("
+        select * from staff
+        ");
+        echo '<br>direct:'.$this->db->last_query();
+        return $query->result();
+    }
+
+    function demojoin(){
+        $this->db->join('store', 'store.store_id = staff.store_id');
+        $query = $this->db->get('staff');
+        echo '<br>join:'.$this->db->last_query();
+        return $query->result();
+    }
 }
